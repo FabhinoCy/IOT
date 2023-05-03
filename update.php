406fe051d685c803c1b4848eaeab7c69669ca221
Fabien CURFS <fabien.curfs@etudiant.univ-reims.fr>
2023-05-03 14:29:25 +0200

    update.php

--- update.php
+++ update.php
@@ -1,22 +1,11 @@
 <?php
-
 header('Access-Control-Allow-Origin: *');
 
-$dsn = 'mysql:dbname=mmi20d07;host=localhost';
-$user = 'mmi20d07';
-$password = 'Password52$';
+$dsn = 'mysql:dbname=iot;host=localhost';
+$user = 'root';
+$password = '';
 
 $bdd = new PDO($dsn, $user, $password);
-$request = "SELECT * FROM iot where name='iot'";
-$query = $bdd->query($request);
-
-$result = $query->fetchAll();
-$state = $result[0]['state'];
-
-if ($state == 1) {
-    $request = "UPDATE `iot` SET `state` = 0 WHERE `name` = 'laser'";
-} else {
-    $request = "UPDATE `iot` SET `state` = 1 WHERE `name` = 'laser'";
-}
+$request = "INSERT INTO `toto` (`id`, `distance`) VALUES (NULL, '2')";
 
 $bdd->query($request);
